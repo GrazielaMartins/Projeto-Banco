2fd01f0776b97a7d75521c67b43de05a6981438f
Graziela Martins <116605840+GrazielaMartins@users.noreply.github.com>
2023-03-30 21:37:13 -0300

    Add files via upload

---
+++ Banco/src/ContaCorrente.java
@@ -0,0 +1,16 @@
+public class ContaCorrente {
+    int saldo;
+    Cliente c;
+
+
+
+    public void deposita (int valorDeposito){
+        saldo = saldo + valorDeposito;
+
+
+    }
+    public int getSaldo(){
+        return saldo;
+    }
+
+}
